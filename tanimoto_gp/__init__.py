6f5a6c04e7ba51f3b3e75923cd0a29a6cc8ae322
Walter Virany <viranyw@cn-b005.server.mila.quebec>
2025-02-17 16:31:10 -0500

    Fix type hints

--- tanimoto_gp/__init__.py
+++ tanimoto_gp/__init__.py
@@ -14,15 +14,14 @@ class TanimotoGP_Params(NamedTuple):
     raw_noise: jnp.ndarray
 
 
-class ZeroMeanTanimotoGP:
-    def __init__(self, fp_func: Callable[[str], Any], smiles_train: list[str], y_train):
-        super().__init__()
+class BaseTanimotoGP:
+    """Base class for Tanimoto kernel Gaussian Process implementation."""
+
+    def __init__(self, fp_func: Callable[[str], Any], smiles_train: list[str], y_train) -> None:
         self._fp_func = fp_func
         self.set_training_data(smiles_train, y_train)
 
-    def set_training_data(self, smiles_train: list[str], y_train: jnp.ndarray):
-        self._smiles_train = smiles_train
-        self._y_train = jnp.asarray(y_train)
+    def _setup_kernel(self, smiles_train: list[str]) -> None:
         self._fp_train = [self._fp_func(smiles) for smiles in smiles_train]
         self._K_train_train = jnp.asarray(
             [DataStructs.BulkTanimotoSimilarity(fp, self._fp_train) for fp in self._fp_train]
@@ -33,18 +32,17 @@ class ZeroMeanTanimotoGP:
             a=TRANSFORM(params.raw_amplitude),
             s=TRANSFORM(params.raw_noise),
             k_train_train=self._K_train_train,
-            y_train=self._y_train,
+            y_train=self._get_training_targets(),
         )
 
     def predict_f(self, params: TanimotoGP_Params, smiles_test: list[str], full_covar: bool = True) -> jnp.ndarray:
-
-        # Construct kernel matrices
         fp_test = [self._fp_func(smiles) for smiles in smiles_test]
         K_test_train = jnp.asarray([DataStructs.BulkTanimotoSimilarity(fp, self._fp_train) for fp in fp_test])
-        if full_covar:
-            K_test_test = jnp.asarray([DataStructs.BulkTanimotoSimilarity(fp, fp_test) for fp in fp_test])
-        else:
-            K_test_test = jnp.ones((len(smiles_test)), dtype=float)
+        K_test_test = (
+            jnp.asarray([DataStructs.BulkTanimotoSimilarity(fp, fp_test) for fp in fp_test])
+            if full_covar
+            else jnp.ones((len(smiles_test)), dtype=float)
+        )
 
         return kgp.noiseless_predict(
             a=TRANSFORM(params.raw_amplitude),
@@ -52,9 +50,28 @@ class ZeroMeanTanimotoGP:
             k_train_train=self._K_train_train,
             k_test_train=K_test_train,
             k_test_test=K_test_test,
-            y_train=self._y_train,
+            y_train=self._get_training_targets(),
             full_covar=full_covar,
         )
+
+    def _get_training_targets(self) -> jnp.ndarray:
+        raise NotImplementedError
+
+    def predict_y(self, params: TanimotoGP_Params, smiles_test: list[str], full_covar: bool = True) -> jnp.ndarray:
+        raise NotImplementedError
+
+
+class ZeroMeanTanimotoGP(BaseTanimotoGP):
+    """Tanimoto GP implementation with zero mean function."""
+
+    def set_training_data(self, smiles_train: list[str], y_train: jnp.ndarray) -> None:
+        self._smiles_train = smiles_train
+        self._y_train = jnp.asarray(y_train)
+        self._setup_kernel(smiles_train)
+
+    def _get_training_targets(self) -> jnp.ndarray:
+        """Get uncentered training targets"""
+        return self._y_train
 
     def predict_y(self, params: TanimotoGP_Params, smiles_test: list[str], full_covar: bool = True) -> jnp.ndarray:
         mean, covar = self.predict_f(params, smiles_test, full_covar)
@@ -63,3 +80,28 @@ class ZeroMeanTanimotoGP:
         else:
             covar += TRANSFORM(params.raw_noise)
         return mean, covar
+
+
+class TanimotoGP(BaseTanimotoGP):
+    """Tanimoto GP implementation with mean function set to training data mean."""
+
+    def set_training_data(self, smiles_train, y_train) -> None:
+        self._smiles_train = smiles_train
+        self._y_train = jnp.asarray(y_train)
+        self._y_mean = jnp.mean(self._y_train)  # Compute mean of training data
+        self._y_centered = self._y_train - self._y_mean  # Center training data by subtracting mean
+        self._setup_kernel(smiles_train)
+
+    def _get_training_targets(self) -> jnp.ndarray:
+        """Get uncentered training targets"""
+        return self._y_centered
+
+    def predict_y(self, params: TanimotoGP_Params, smiles_test: list[str], full_covar: bool = True) -> jnp.ndarray:
+        """Predict observed values for test points with added mean."""
+
+        mean, covar = self.predict_f(params, smiles_test, full_covar)
+        if full_covar:
+            covar = covar + jnp.eye(len(smiles_test)) * TRANSFORM(params.raw_noise)
+        else:
+            covar += TRANSFORM(params.raw_noise)
+        return mean + self._y_mean, covar
